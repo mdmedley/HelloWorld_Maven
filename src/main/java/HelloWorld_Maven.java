c3fe5b02e606b4ffdbf41ab6bec9385ee4efdb91
mdmedley <mdmeds@gmail.com>
2013-09-11 14:30:45 -0400

    Initial commit
    Attempting to do Hello World application using Maven

---
+++ src/main/java/HelloWorld_Maven.java
@@ -0,0 +1,19 @@
+public class HelloWorld_Maven {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World...");
+		System.out.println("California knows how to party!");
+		System.out.println("In the city...city of Compton!!");
+		System.out.println(getMessage());
+
+	}
+	
+	public static String getMessage() {
+		return "We keep it rockin!";
+	}
+
+}
