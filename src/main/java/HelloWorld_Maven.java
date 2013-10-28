7e7416ccf742ce219120a695063749024755f90e
mdmedley <mdmeds@gmail.com>
2013-10-28 15:21:48 -0400

    Added flavor message

--- src/main/java/HelloWorld_Maven.java
+++ src/main/java/HelloWorld_Maven.java
@@ -9,6 +9,9 @@ public class HelloWorld_Maven {
 		System.out.println("California knows how to party!");
 		System.out.println("In the city...city of Compton!!");
 		System.out.println(getMessage());
+		int flavors = 23;
+		String message = "There are " + flavors + " available flavors";
+		System.out.println(message);
 
 	}
 	
